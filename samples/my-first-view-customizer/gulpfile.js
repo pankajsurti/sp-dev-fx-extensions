e193f1993d3cd8b930b5344e5ebea46a2e4c07f2
Pankaj Surti <pankajsurti@gmail.com>
2020-05-24 11:51:01 -0500

    added addSuppression

--- samples/my-first-view-customizer/gulpfile.js
+++ samples/my-first-view-customizer/gulpfile.js
@@ -8,6 +8,17 @@ build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not came
 
 build.initialize(require('gulp'));
 
+// copied from
+// https://spblog.net/post/2019/01/02/sharepoint-framework-development-some-gotchas-and-how-to-solve-them
+let args = build.getConfig().args;
+let isProductionBundle = args._.indexOf('bundle') !== -1 && (args.ship || args.production || args.p);
+
+if (isProductionBundle) {
+  build.addSuppression(/Warning - \[sass\] The local CSS class/gi);
+  // OR
+  build.addSuppression(/Warning/gi);
+}
+
 // Copied from 
 // https://www.eliostruyf.com/how-to-let-the-warnings-not-fail-the-sharepoint-framework-build-process/
 // Retrieve the current build config and check if there is a `warnoff` flag set
