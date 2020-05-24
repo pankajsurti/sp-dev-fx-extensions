3811800f597698445baa1573f19a8a7e178f78f4
Pankaj Surti <pankajsurti@gmail.com>
2020-05-24 10:58:23 -0500

    shouldWarningsFailBuild: false

--- samples/my-first-view-customizer/gulpfile.js
+++ samples/my-first-view-customizer/gulpfile.js
@@ -5,3 +5,26 @@ const build = require('@microsoft/sp-build-web');
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
 
 build.initialize(require('gulp'));
+
+// Copied from 
+// https://www.eliostruyf.com/how-to-let-the-warnings-not-fail-the-sharepoint-framework-build-process/
+// Retrieve the current build config and check if there is a `warnoff` flag set
+const crntConfig = build.getConfig();
+const warningLevel = crntConfig.args["warnoff"];
+
+// Extend the SPFx build rig, and overwrite the `shouldWarningsFailBuild` property
+if (warningLevel) {
+  class CustomSPWebBuildRig extends build.SPWebBuildRig {
+    setupSharedConfig() {
+      build.log("IMPORTANT: Warnings will not fail the build.")
+      build.mergeConfig({
+        shouldWarningsFailBuild: false
+      });
+      super.setupSharedConfig();
+    }
+  }
+
+  build.rig = new CustomSPWebBuildRig();
+}
+
+build.initialize(gulp);
