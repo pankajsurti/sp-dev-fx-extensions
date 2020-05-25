37a3e18b8381a051c5de0a850379450b887db15e
Pankaj Surti <pankajsurti@gmail.com>
2020-05-25 12:48:02 -0500

    fixed error no-use-before-declare: variable 'classNames' used before declaration

--- samples/react-application-page-related-bing-news/src/extensions/pageRelatedBingNews/components/RelatedBingNews.tsx
+++ samples/react-application-page-related-bing-news/src/extensions/pageRelatedBingNews/components/RelatedBingNews.tsx
@@ -20,6 +20,63 @@ export interface IRelatedBingNewsState {
   showPanel: boolean;
 }
 
+interface IListBasicExampleClassObject {
+  itemCell: string;
+  itemImage: string;
+  itemContent: string;
+  itemName: string;
+  itemIndex: string;
+  chevron: string;
+}
+
+const theme: ITheme = getTheme();
+const { palette, semanticColors, fonts } = theme;
+
+const classNames: IListBasicExampleClassObject = mergeStyleSets({
+  itemCell: [
+    getFocusStyle(theme, { inset: -1 }),
+    {
+      minHeight: 54,
+      padding: 10,
+      boxSizing: 'border-box',
+      borderBottom: `1px solid ${semanticColors.bodyDivider}`,
+      display: 'flex',
+      selectors: {
+        '&:hover': { background: palette.neutralLight }
+      }
+    }
+  ],
+  itemImage: {
+    flexShrink: 0
+  },
+  itemContent: {
+    marginLeft: 10,
+    overflow: 'hidden',
+    flexGrow: 1
+  },
+  itemName: [
+    fonts.xLarge,
+    {
+      whiteSpace: 'nowrap',
+      overflow: 'hidden',
+      textOverflow: 'ellipsis'
+    }
+  ],
+  itemIndex: {
+    fontSize: fonts.small.fontSize,
+    color: palette.neutralTertiary,
+    marginBottom: 10
+  },
+  chevron: {
+    alignSelf: 'center',
+    marginLeft: 10,
+    color: palette.neutralTertiary,
+    fontSize: fonts.large.fontSize,
+    flexShrink: 0
+  }
+});
+
+
 export default class RelatedBingNews extends React.Component<IRelatedBingNewsProps, IRelatedBingNewsState> {
 
   private _baseUrl: string;
@@ -160,58 +217,3 @@ export default class RelatedBingNews extends React.Component<IRelatedBingNewsPro
   }
 }
 
-interface IListBasicExampleClassObject {
-  itemCell: string;
-  itemImage: string;
-  itemContent: string;
-  itemName: string;
-  itemIndex: string;
-  chevron: string;
-}
-
-const theme: ITheme = getTheme();
-const { palette, semanticColors, fonts } = theme;
-
-const classNames: IListBasicExampleClassObject = mergeStyleSets({
-  itemCell: [
-    getFocusStyle(theme, { inset: -1 }),
-    {
-      minHeight: 54,
-      padding: 10,
-      boxSizing: 'border-box',
-      borderBottom: `1px solid ${semanticColors.bodyDivider}`,
-      display: 'flex',
-      selectors: {
-        '&:hover': { background: palette.neutralLight }
-      }
-    }
-  ],
-  itemImage: {
-    flexShrink: 0
-  },
-  itemContent: {
-    marginLeft: 10,
-    overflow: 'hidden',
-    flexGrow: 1
-  },
-  itemName: [
-    fonts.xLarge,
-    {
-      whiteSpace: 'nowrap',
-      overflow: 'hidden',
-      textOverflow: 'ellipsis'
-    }
-  ],
-  itemIndex: {
-    fontSize: fonts.small.fontSize,
-    color: palette.neutralTertiary,
-    marginBottom: 10
-  },
-  chevron: {
-    alignSelf: 'center',
-    marginLeft: 10,
-    color: palette.neutralTertiary,
-    fontSize: fonts.large.fontSize,
-    flexShrink: 0
-  }
-});
